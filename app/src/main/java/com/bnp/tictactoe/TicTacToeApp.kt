98e5cc5aa510aa2840df2128f3847937bc991647
Chloe Vanhee <chloe.vanhee@gmail.com>
2026-02-28 15:28:12 +0100

    refactor: use Koin DI

--- app/src/main/java/com/bnp/tictactoe/TicTacToeApp.kt
+++ app/src/main/java/com/bnp/tictactoe/TicTacToeApp.kt
@@ -1,4 +1,18 @@
 package com.bnp.tictactoe
 
-class TicTacToeApp {
+import android.app.Application
+import com.bnp.tictactoe.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class TicTacToeApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@TicTacToeApp)
+            modules(appModule)
+
+        }
+    }
 }
--- app/src/main/java/com/bnp/tictactoe/di/appModule.kt
+++ app/src/main/java/com/bnp/tictactoe/di/appModule.kt
@@ -1,2 +1,13 @@
 package com.bnp.tictactoe.di
 
+import com.bnp.tictactoe.domain.usecases.PlayTurnUseCase
+import com.bnp.tictactoe.domain.usecases.PlayTurnUseCaseInterface
+import com.bnp.tictactoe.presentation.GameViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.dsl.module
+
+
+val appModule = module {
+    single<PlayTurnUseCaseInterface> { PlayTurnUseCase() }
+    viewModelOf(::GameViewModel)
+}
